8a3d66da4881cff4e83936a6f12692708c5171f2
yousuke-shiraishi <y-shiraishi@nexyzbb.ne.jp>
2017-12-06 20:38:28 +0900

    詳細画面に変更画面を表示するようにする

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-  process :resize_to_limit => [200, 200] # 画像サイズの調整
+  process :resize_to_limit => [300, 300] # 画像サイズの調整
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
